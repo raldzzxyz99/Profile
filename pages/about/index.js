6be4f5217eaa07e7a21ecd5216b403c0f16fdf9a
N-ꓘiuuR <kyuurzy@gmail.com>
2024-11-08 05:33:58 +0800

    Update index.js

--- pages/about/index.js
+++ pages/about/index.js
@@ -26,7 +26,7 @@ const About = () => {
 
         <motion.div {...ContentAnimation} className="relative order-1 my-10 w-3/4 text-justify md:order-2 md:my-0 md:w-1/4">
           <p className="text-md font-light text-primary-light">
-            {`Hello! My name is KyuuRzy, you can call me Kyuu, Kyy, iky or whatever you want. I'm a Front-end Developer located in South Sulawesi, Indonesia. As a Front-end Develover I used to code websites from stratch. I love to create simple yet beautiful websites with great user experience.`}
+            {`Hello! My name is N-ꓘiuuR, you can call me Kyuu, or whatever you want. I'm a beginners who are learning JavaScript, located in South Sulawesi, Indonesia. As a Front-end Develover I used to code websites from stratch. I love to create simple yet beautiful websites with great user experience.`}
           </p>
         </motion.div>
       </section>
