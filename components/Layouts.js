a5bc4a854dddcc2da5250a88e5e41f82f185f221
raldzzxyz99 <jastebraldzz@gmail.com>
2025-04-04 20:04:56 +0800

    Update Layouts.js

--- components/Layouts.js
+++ components/Layouts.js
@@ -5,7 +5,7 @@ const Layouts = (props) => {
   return (
     <>
       <Head>
-        <title>My Portfolio{pageTitle}</title>
+        <title>Portofolio RaldzzXyz{pageTitle}</title>
       </Head>
 
       <>{children}</>
