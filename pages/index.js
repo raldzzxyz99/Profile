4541ed08ed512d57b8db57f41663e840b72a7464
raldzzxyz99 <jastebraldzz@gmail.com>
2025-04-04 12:26:09 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,7 @@ const Home = () => {
 
         <motion.div {...ContentAnimation} className="relative order-1 w-1/3 md:order-2">
           <h3 className="-mb-5 text-base font-normal">Hi! My Name Is
+ 
   </h3>
           <h1 className="-ml-1 mb-1 text-7xl font-bold text-primary-light">ЯXyz</h1>
           <h4 className="text-sm font-light transition-all duration-500 hover:text-primary-light">Learner JavaScript</h4>
