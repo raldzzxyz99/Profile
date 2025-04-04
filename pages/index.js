b5f91cd6df807d8a2563302b251736a8764f5242
raldzzxyz99 <jastebraldzz@gmail.com>
2025-04-04 12:19:42 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Home = () => {
     <Layouts>
       <section className="relative flex w-full flex-col items-center justify-evenly py-40 md:h-screen md:flex-row md:overflow-hidden">
         <motion.span {...FadeAnimation} className="title-page">
-          ꓘiuuR
+          RaldzzXyz
         </motion.span>
 
         <motion.div {...ContentAnimation} className="relative order-2 flex w-1/2 justify-end md:order-1 md:w-1/3">
@@ -22,9 +22,9 @@ const Home = () => {
         </motion.div>
 
         <motion.div {...ContentAnimation} className="relative order-1 w-1/3 md:order-2">
-          <h3 className="-mb-5 text-base font-normal">Hi! My name is</h3>
-          <h1 className="-ml-1 mb-1 text-7xl font-bold text-primary-light">ꓘiuuR</h1>
-          <h4 className="text-sm font-light transition-all duration-500 hover:text-primary-light">Learn JavaScript</h4>
+          <h3 className="-mb-5 text-base font-normal">Hi! My Name Is</h3>
+          <h1 className="-ml-1 mb-1 text-7xl font-bold text-primary-light">RaldzzXyz</h1>
+          <h4 className="text-sm font-light transition-all duration-500 hover:text-primary-light">Learner JavaScript</h4>
         </motion.div>
       </section>
     </Layouts>
